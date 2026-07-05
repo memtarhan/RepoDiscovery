1096d7542e0cb97c3fa973bdd3096cbbb5799259
Mehmet Tarhan <hi@memtarhan.com>
2026-07-06 00:07:51 +0300

    <dev>: create project

---
+++ RepoDiscovery/RepoDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoDiscoveryApp.swift
+//  RepoDiscovery
+//
+//  Created by Mehmet Tarhan on 6.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct RepoDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
